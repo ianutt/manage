525cd2d3ec51d3bed0cb1f1ee9d74ef5adbc6cd9
ianutt <930190962@qq.com>
2022-03-07 22:47:40 +0800

    首页开始

--- views/home/Home.vue
+++ views/home/Home.vue
@@ -1,10 +1,34 @@
 <template>
-  <h1>我是Home</h1>
+  <el-row class="home" :gutter="20">
+    <el-col :span="8" style="margin-top: 20px">
+      <el-card shadow="hover">
+        <div class="user">
+          <img :src="useImg" alt="">
+          <div class="userinfo">
+            <p class="name">Admin</p>
+            <p class="access">超级管理员</p>
+          </div>
+        </div>
+        <div class="login-info">
+          <p>上次登录时间：<span>2022-2-22</span></p>
+          <p>上次登录地点：<span>北京</span></p>
+        </div>
+      </el-card>
+      <el-card style="margin-top: 20px; height: 460px;">
+
+      </el-card>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
 export default {
-  name: "Home"
+  name: "Home",
+  data() {
+    return {
+      useImg: require('../../src/assets/images/user.png')
+    }
+  }
 }
 </script>
 
